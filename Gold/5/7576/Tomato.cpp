3e39982a5157a355be91167993006f73248226c4
JMEspoir <maro3534@knu.ac.kr>
2023-04-07 00:07:31 +0900

    fix

--- Gold/5/7576/Tomato.cpp
+++ Gold/5/7576/Tomato.cpp
@@ -2,47 +2,16 @@
 #include <vector>
 #include <utility>
 #include <queue>
+
 using namespace std;
 
 int arr[4][2] = {{1,0},{-1,0},{0,-1},{0,1}};
 int zerocount =0;
+
 queue<pair<int,int>> q;
 
-bool fun(int r,int c,int row, int col){
-  if(r<0 || r>=row){
-    return false;
-  }
-  if(c<0 || c>=col){
-    return false;
-  }
-  return true;
-}
-
-int BFS(int row, int col, vector<vector<int>> v){
-  int count =0;
-  pair<int,int> tem;
-  bool exit = true;
-  int size =0;
-  
-  while(exit){
-    count ++;
-    exit=false;
-    size = q.size();
-    for(int i=0;i<size;i++){
-      tem = q.front();
-      q.pop();
-      for(int j=0;j<4;j++){
-        if(fun(tem.first+arr[j][0],tem.second+arr[j][1],row,col) && v[tem.first+arr[j][0]][tem.second+arr[j][1]]==0){
-          q.push(make_pair(tem.first+arr[j][0],tem.second+arr[j][1]));
-          v[tem.first+arr[j][0]][tem.second+arr[j][1]] = 1;
-          zerocount --;
-          exit = true;
-        }
-      }
-    }
-  }
-  return count;
-}
+bool isTrue(int r,int c,int row, int col);
+int BFS(int row, int col, vector<vector<int>> v);
 
 int main(void){
   int row,col;
@@ -50,7 +19,6 @@ int main(void){
   int minuscount=0;
 
   cin >> col >> row;
-
   vector<vector<int>> v(row,vector<int>(col));
   for(int i=0;i<row;i++){
     for(int j=0;j<col;j++){
@@ -76,7 +44,9 @@ int main(void){
     cout << -1;
     return 0;
   }
+
   int count = BFS(row,col,v);
+
   if(zerocount){
     cout << -1;
     return 0;
@@ -86,8 +56,39 @@ int main(void){
     return 0;
   }
   
-  
+}
 
+bool isTrue(int r,int c,int row, int col){
+  if(r<0 || r>=row){
+    return false;
+  }
+  if(c<0 || c>=col){
+    return false;
+  }
+  return true;
+}
 
-
+int BFS(int row, int col, vector<vector<int>> v){
+  int count =0;
+  pair<int,int> tem;
+  int size =q.size();
+  int tr,tc;
+  while(size){
+    count ++;
+    for(int i=0;i<size;i++){
+      tem = q.front();
+      q.pop();
+      for(int j=0;j<4;j++){
+        tr = tem.first+arr[j][0];
+        tc = tem.second+arr[j][1];
+        if(isTrue(tr,tc,row,col) && v[tr][tc]==0){
+          q.push(make_pair(tr,tc));
+          v[tr][tc] = 1;
+          zerocount --;
+        }
+      }
+    }
+    size=q.size();
+  }
+  return count;
 }
